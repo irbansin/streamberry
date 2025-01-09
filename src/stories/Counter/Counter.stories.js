68b0addad14cd98425cc93b13b12410117db2df9
Anirban <reach@anirbansinha.in>
2025-01-10 04:59:37 +0530

    counter story added to storybook

---
+++ src/stories/Counter/Counter.stories.js
@@ -0,0 +1,13 @@
+import Counter from "./Counter.tsx";
+
+export default {
+  title: "Components/Counter",
+  component: Counter,
+  tags: ["autodocs"],
+};
+
+export const Default = {
+  args: {
+    count: 0,
+  },
+};
